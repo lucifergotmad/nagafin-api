288c5701ad793537e3c346842332b32d06a3b2a2
lucifergotmad <amateur.fullstackdev@gmail.com>
2022-09-02 11:23:45 +0700

    fix:[major] adding refresh token and many more

--- src/modules/app/controller/dtos/auth-refresh-token.dto.ts
+++ src/modules/app/controller/dtos/auth-refresh-token.dto.ts
@@ -1,9 +1,9 @@
 import { IsRequiredString } from 'src/core/decorators/dto-decorators/required-string.decorator';
 
 export class AuthRefreshTokenRequestDTO {
-  @IsRequiredString()
+  @IsRequiredString({ example: 'lucifer' })
   username: string;
 
-  @IsRequiredString()
+  @IsRequiredString({ example: '23498sdf98234-23498ydsf-23823h-sd8f324' })
   refresh_token: string;
 }
